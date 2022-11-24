88c41d9578251c554f9a41ece6b0167ad43b2428
Alex <americantragedy77221@gmail.com>
2022-11-24 16:42:41 +0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 21500;
+        int rub = 20;
+        int bonus = price / rub;
+        System.out.println("Вам начислено " + bonus + " бонусных миль.");
+
+    }
+}
